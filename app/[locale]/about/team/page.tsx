fed5e33ee69cfd82b94965ddd4134edc766eca35
tibogot <tibogotbeatz@gmail.com>
2026-01-15 00:29:31 +0100

    ipz

--- app/[locale]/about/team/page.tsx
+++ app/[locale]/about/team/page.tsx
@@ -14,12 +14,15 @@ export default async function TeamPage() {
         </div>
         {/* Content Overlay */}
         <div className="relative z-10 w-full max-w-5xl mx-auto text-center">
-          <h1 className="text-4xl md:text-7xl font-light leading-tight text-white mb-4 md:mb-6 font-pp-neue-montreal">
+          <h1 className="text-4xl md:text-7xl font-normal leading-tight text-white mb-4 md:mb-6 font-pp-neue-montreal">
             {t("title")}
           </h1>
-          <p className="text-lg max-w-xl mx-auto md:text-lg text-white font-pp-neue-montreal">
+          <p className="text-lg max-w-xl mx-auto md:text-lg text-white font-pp-neue-montreal mb-6 md:mb-8">
             {t("description")}
           </p>
+          <button className="px-8 py-2 bg-[#8202FF] cursor-pointer text-white font-pp-neue-montreal rounded-sm hover:bg-[#6a02cc] transition-colors">
+            {t("cta")}
+          </button>
         </div>
       </section>
     </div>
