21c245209cb95cad9be55901565c61421e1795f3
tibogot <tibogotbeatz@gmail.com>
2026-01-24 03:11:47 +0100

    eip

--- app/[locale]/about/mission/page.tsx
+++ app/[locale]/about/mission/page.tsx
@@ -1,14 +1,19 @@
-import PinnedImageReveal from '@/app/components/PinnedImageReveal';
+import { getTranslations } from "next-intl/server";
+import ManagedServicesHero from "@/app/components/ManagedServicesHero";
 
-export default function MissionPage() {
+export default async function MissionPage() {
+  const t = await getTranslations("about.mission");
+
   return (
-    <>
-      <PinnedImageReveal
-        imageSrc="/img-1.jpg"
-        imageAlt="Mission statement background"
-        useLogo={true}
+    <div className="w-full min-h-screen bg-black">
+      {/* Hero Section (same concept as Managed Services) */}
+      <ManagedServicesHero
+        title={t("title")}
+        description={t("description")}
+        imageSrc="/mario-gogh.jpg"
+        imageAlt="Mission Statement Hero"
       />
-    </>
+    </div>
   );
 }
 
